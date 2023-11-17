3d5036db3df9455dd9d18bc14cce8ec9b48d4e68
gzy02 <1262454489@qq.com>
2023-11-17 09:38:21 +0800

    4_chat data.json

--- evaluate.sh
+++ evaluate.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
     --stage sft \
     --model_name_or_path /raid/gzy/chatglm3/chatglm3-6b/ \
     --do_predict \
-    --dataset annotation_dev \
+    --dataset 4_chat_dev \
     --template chatglm3 \
     --finetuning_type lora \
     --checkpoint_dir /raid/gzy/chatglm3/checkpoint_4/checkpoint-$i \
