01858573fca3a9fcb63fbccc253eb66078c4523d
gzy02 <1262454489@qq.com>
2023-11-17 13:25:12 +0800

    point_2

--- evaluate.sh
+++ evaluate.sh
@@ -1,15 +1,15 @@
 
-for i in $(seq 10 10 360)
+for i in $(seq 110 10 240)
 do
-CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
+CUDA_VISIBLE_DEVICES=2 python src/train_bash.py \
     --stage sft \
     --model_name_or_path /raid/gzy/chatglm3/chatglm3-6b/ \
     --do_predict \
-    --dataset 4_chat_dev \
+    --dataset point_dev \
     --template chatglm3 \
     --finetuning_type lora \
-    --checkpoint_dir /raid/gzy/chatglm3/checkpoint_4/checkpoint-$i \
-    --output_dir chat_4/instruction_4/$i \
+    --checkpoint_dir /raid/gzy/chatglm3/checkpoint_point_2/checkpoint-$i \
+    --output_dir point_2/point_0/$i \
     --per_device_eval_batch_size 16 \
     --predict_with_generate
 done
